9762a100c97ffa911d878b715a0159d85b1651ff
aeli kim <aelli2002@naver.com>
2024-04-04 21:27:22 +0900

    최종

--- Problem1.js
+++ Problem1.js
@@ -6,7 +6,7 @@ let Name = "주용";
 
 alert(`hello ${1}`); // hello 1
 
-alert(`hello ${"Name"}`); // hello "Name"
+alert(`hello ${"Name"}`); // hello Name
 
 alert("hello ${Name}"); // hello ${Name}
 
